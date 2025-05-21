fa976364fa4d2179a392a799add9b8fa1ff973d3
satchmo1124 <peterkim7777@naver.com>
2025-05-21 17:21:50 +0900

    added postprocessor that tages album and album artist

--- main.py
+++ main.py
@@ -3,7 +3,35 @@ import tkinter.font
 from tkinter import filedialog,messagebox
 import yt_dlp
 import threading 
-import eyed3
+from yt_dlp.postprocessor.ffmpeg import FFmpegMetadataPP
+
+    
+class CustomMetadataPP(FFmpegMetadataPP):
+    def __init__(self,downloader=None,album_name=None,artist_name=None):
+        super().__init__(downloader)
+        self._album_name = album_name
+        self._artist_name = artist_name
+    def run(self,info):
+
+        # for key,val in info.items():
+        #     if len(str(val)) > 30:
+        #         continue
+        #     print(f'{key} : {val}')
+
+        if 'filepath' not in info:
+            filepath = self._downloader.prepare_filename(info)
+            info['filepath'] = filepath.replace('webm','mp3')
+
+        if 'album' not in info:
+            info['album'] = self._album_name or 'Unknown Album'
+
+        if 'artist' not in info:
+            info['artist'] = self._artist_name or info['uploader']
+
+        if 'album_artist' not in info:
+            info['album_artist'] = self._artist_name or info['uploader']
+
+        return super().run(info)
 
 class YoutubeDownloader:
     def __init__(self,root):
@@ -46,9 +74,147 @@ class YoutubeDownloader:
 
 
         self.user_options = tk.Frame(root)
-        self.user_options.pack()
+        self.user_options.pack(pady=5)
 
-        self.artist_label = tk.Label(self.user_options,text= "Artist Name:")
+        self.artist_label = tk.Label(self.user_options,text="Artist Name (only mp3):")
+        self.artist_label.pack(pady=3)
+
+        self.artist_entry = tk.Entry(self.user_options,width=30)
+        self.artist_entry.pack(pady=3)
+
+        self.album_label = tk.Label(self.user_options,text = "Album Name (only mp3):")
+        self.album_label.pack(pady=3)
+
+        self.album_entry = tk.Entry(self.user_options,width=30)
+        self.album_entry.pack(pady=3)
+
+
+        # Download Button Frame
+
+        self.download_button_mp4 = tk.Button(root, text="Download Video (MP4)", command=lambda: self.download_media('mp4'))
+        self.download_button_mp4.pack(pady=10)
+
+        self.download_button_mp3 = tk.Button(root, text="Download Audio (MP3)", command=lambda: self.download_media('mp3'))
+        self.download_button_mp3.pack(pady=5)
+
+        self.root.bind('<Return>',lambda event: self.download_button_mp4.invoke())
+        self.root.bind('<Return>',lambda event: self.download_button_mp3.invoke())
+
+        # Status 
+        self.status_label = tk.Label(root,text="")
+        self.status_label.pack(pady=10)
+
+    def download_media(self,format):
+        url = self.url_entry.get()
+        if not url:
+            messagebox.showerror("URL Error","Type appropriate URL.")
+            return
+        
+        self.download_button_mp4.config(state="disabled")
+        self.download_button_mp3.config(state="disabled")
+        self.status_label.config(text="downloading...")
+
+        ydl_opts = {}
+
+        if format == "mp4" :
+            ydl_opts = {
+                'format' : 'bestvideo*+bestaudio/best',
+                'writethumbnail' : True ,
+                'quiet' : True,
+            }
+
+        if format == 'mp3':
+            ydl_opts = {
+                'format' : 'bestaudio/best',
+                'convert_thumbnails' : 'jpg',
+                #'verbose' : True, # Print additional info to stdout.
+                'quiet' : True,
+                'postprocessors' : [{
+                    'key': 'FFmpegExtractAudio',
+                    'preferredcodec': 'mp3',
+                    'preferredquality': '320', # bitrate of the audio file that ffmpeg will produce
+                    },
+                    { 'key' : 'EmbedThumbnail' },
+                    ],
+                'writethumbnail' : True, # Write the thumbnail image to a file 
+                'embedthumbnail' : True,
+            }        
+        def run_download(ydl_opts,format):
+            try:
+                save_path = filedialog.askdirectory()
+                if not save_path:
+                    raise Exception("Save Path not selected.")
+                
+                ydl_opts['outtmpl'] = f'{save_path}/%(title)s.%(ext)s'
+             
+                with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                    info = ydl.extract_info(url,download=True)
+
+                    # CustomMetadata PostProcessing
+                    if format == 'mp3':
+                        metadata_pp = CustomMetadataPP(ydl,
+                                                   album_name=self.album_entry.get(),
+                                                   artist_name=self.artist_entry.get())
+                        metadata_pp.run(info)
+                
+
+                self.status_label.config(text="Download Finished")
+
+            except Exception as e:
+                messagebox.showerror("Download Error",f"Download failed : {e}")
+                self.status_label.config(text="")
+            
+            finally:
+                self.download_button_mp4.config(state="normal")
+                self.download_button_mp3.config(state='normal')
+
+        threading.Thread(target=lambda: run_download(ydl_opts,format)).start()
+
+
+if __name__ == "__main__":
+    #help(yt_dlp.YoutubeDL)
+    root = tk.Tk()
+    app = YoutubeDownloader(root)
+    root.mainloop()
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+
+     self.artist_label = tk.Label(self.user_options,text= "Artist Name:")
         self.artist_label.pack()
 
         self.artist_entry = tk.Entry(self.user_options,width = 50)
@@ -67,89 +233,19 @@ class YoutubeDownloader:
         # self.user_thumbnail.pack()
 
 
+'''
 
-        # Download Button Frame
+                #     { 
+                #         'key' : 'FFmpegMetadata',
+                #         'add_metadata' : True,
+                #     } ],
+                # 'postprocessor_args': [
+                #     '-metadata',f"album={self.album_entry.get()}",
+                #     '-metadata',f'album_artist=%(uploader)s',
+                # ],
 
-        self.download_button_mp4 = tk.Button(root, text="Download Video (MP4)", command=lambda: self.download_media('mp4'))
-        self.download_button_mp4.pack(pady=10)
-
-
-
-        self.download_button_mp3 = tk.Button(root, text="Download Audio (MP3)", command=lambda: self.download_media('mp3'))
-        self.download_button_mp3.pack(pady=5)
-
-        # Status 
-        self.status_label = tk.Label(root,text="")
-        self.status_label.pack(pady=10)
-
-    def artist_as_album_artist(self):
-        self.album_artist_entry.delete(0,tk.END)
-        self.album_artist_entry(0,self.artist_entry.get())
-
-
-
-    def download_media(self,format):
-        url = self.url_entry.get()
-        if not url:
-            messagebox.showerror("URL Error","Type appropriate URL.")
-            return
-        
-        self.download_button_mp4.config(state="disabled")
-        self.download_button_mp3.config(state="disabled")
-        self.status_label.config(text="downloading...")
-
-        
-
-        ydl_opts = {}
-
-        if format == "mp4" :
-            ydl_opts['format'] = 'bestvideo*+bestaudio/best'
-            ydl_opts['writethumbnail'] = True
-            ydl_opts['quiet'] = True
-            
-        
-
-        if format == 'mp3':
-            ydl_opts['format'] = 'bestaudio/best'
-            #ydl_opts['writethumbnail'] = True
-            #ydl_opts['verbose'] = True  # Print additional info to stdout. 
-            #ydl_opts['list_thumbnails'] = True
-            ydl_opts['convert_thumbnails'] = 'jpg'
-            ydl_opts['writethumbnail'] = True
-            ydl_opts['embedthumbnail'] = True
-            ydl_opts['postprocessors'] = [{
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'mp3',
-                'preferredquality': '192', # bitrate of the audio file that ffmpeg will produce
-            },
-            {
-                'key' : 'EmbedThumbnail'
-            },
-            {
-                'key' : 'FFmpegMetadata'
-            }
-            ]
-
-
-        def run_download(ydl_opts):
-            try:
-                save_path = filedialog.askdirectory()
-                if not save_path:
-                    raise Exception("Save Path not selected.")
-                
-                ydl_opts['outtmpl'] = f'{save_path}/%(title)s.%(ext)s'
-             
-                with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-                    ydl.download([url])
-
-                self.status_label.config(text="Download Finished")
-
-            except Exception as e:
-                messagebox.showerror("Download Error",f"Download failed:{e}")
-                self.status_label.config(text="")
-            
-            finally:
-                file = ydl_opts['outtmpl']
+'''
+                file = f'{save_path}/%(title)s.%(ext)s'
                 audiofile = eyed3.load(file)
 
                 if audiofile.tag is None:
@@ -161,33 +257,4 @@ class YoutubeDownloader:
 
                     if self.album_artist_entry.get() is None:
                         audiofile.tag.album_artist = audiofile.tag.artist
-
-                
-                self.download_button_mp4.config(state="normal")
-                self.download_button_mp3.config(state='normal')
-
-        threading.Thread(target=lambda: run_download(ydl_opts)).start()
-
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    #help(yt_dlp.YoutubeDL)
-
-    app = YoutubeDownloader(root)
-    root.mainloop()
-        
-
-'''
-
-
-# if format == "mp4":
-            ydl_opts = {
-            'format' : 'bestvideo+bestaudio/best',
-            'quiet':True, # Do not print messages to stdout. 
-                #'writeinfojson':True, 
-                #'getcomments':True, # get comment from the video
-            
-            }
-
-
-'''
+                '''
